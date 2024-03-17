0fce6327b3b02118e611fb5e77b8c725579dd8ab
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-17 12:56:50 +0400

    added docker and gunicorn

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,7 +1,7 @@
 
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,6 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')# es davamate statikuri filebis shesanaxad
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
