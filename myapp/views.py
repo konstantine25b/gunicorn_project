002928a76adf897a0c0b9bad011931ddf10d0164
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-17 12:13:39 +0400

    added serialziers viewsets

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class= ItemSerializer 
